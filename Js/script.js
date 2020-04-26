156b29277d65c912c68004100e38b55691bce7a6
Fikra <ficra725@gmail.com>
2020-04-26 18:39:10 +0700

    Penambahan responsive cerdas
    penambahan click scrool dengan memanfaatkan JQueri dan Jqueri easing.

---
+++ Js/script.js
@@ -0,0 +1,43 @@
+
+
+// ini awal page scroll
+$('.page-scroll').on('click', function(e){
+	// console.log('ok');
+	// ambil isi href
+	var tujuan = $(this).attr('href');
+	// console.log(tujuan);
+
+
+ //tangkap element ybs 
+	var elementTujuan = $(tujuan);
+	// console.log(elementTujuan.offset().top);
+	// console.log($('html,body').scrollTop());
+
+//pindahkan scroll
+	$('html,body').animate({
+		scrollTop: elementTujuan.offset().top-70
+	}, 1000,'easeInOutExpo'); //ada dua effek swing dan linier dari jquery..kalau bosan kita bisa pake transisi yang lain buka aa website query easig link: gsgd.com
+	e.preventDefault();
+});
+// ini akhir page scroll
+
+
+
+// ini awal windows scroll
+$(window).scroll(function(){
+	var wScroll=$(this).scrollTop();
+	$('.copy-container h1').css({
+		'transform':'translate(0px,'+wScroll/4.5+'%)'
+	});
+	$('.copy-container h2').css({
+		'transform':'translate(0px,'+wScroll/6+'%)'
+	});
+
+	console.log(wScroll);
+});
+// ini akhir windows scroll
+
+
+
+
+
