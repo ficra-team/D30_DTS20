fd2e124f295f7b0b2ce79740947f2185799d7276
Fikra <ficra725@gmail.com>
2020-04-26 18:52:26 +0700

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -27,13 +27,15 @@ $('.page-scroll').on('click', function(e){
 $(window).scroll(function(){
 	var wScroll=$(this).scrollTop();
 	$('.copy-container h1').css({
-		'transform':'translate(0px,'+wScroll/4.5+'%)'
+		'transform':'translate(0px,'+wScroll/5.5+'%)'
 	});
 	$('.copy-container h2').css({
 		'transform':'translate(0px,'+wScroll/6+'%)'
 	});
+	
 
-	console.log(wScroll);
+
+
 });
 // ini akhir windows scroll
 
